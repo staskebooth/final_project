3711731b9f720d2dd0a38a50ff7b5e824be05c74
staskebooth <jstaske@chicagobooth.edu>
2021-06-03 22:43:35 +0000

    login system works

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :username, presence: true
   validates :password, presence: true
 end
